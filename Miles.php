9146c822d397154dbda16cb2371409de2decbfc0
loicDEGLA <houegnon.degla@groupe-esigelec.org>
2023-10-19 11:50:58 +0200

    Je suis le second de surcroit le plus dope

---
+++ Miles.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I'm the black spiderman</title>
+</head>
+<body>
+
+        <p> My name is Miles Morales and i am </p>
+        <h2>The new Spiderman!</h2>
+        
+</body>
+</html>
