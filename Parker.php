79f08614dd04eafb1d7d1af600f1184e0770eb7c
loicDEGLA <houegnon.degla@groupe-esigelec.org>
2023-10-19 11:50:27 +0200

    Je suis le premier spiderman

---
+++ Parker.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I'm spiderman</title>
+</head>
+<body>
+
+    <p> My name is Peter parker and i am </p>
+    <h2>Spiderman!</h2>
+    
+</body>
+</html>
